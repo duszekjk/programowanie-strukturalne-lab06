7f81a83fa44eb9343f51b1985a40d92844726a88
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-06-11 13:28:06 +0200

    Fix truncated game mode source

--- Source/IpcCharacterWorld/Private/IpcCharacterWorldGameMode.cpp
+++ Source/IpcCharacterWorld/Private/IpcCharacterWorldGameMode.cpp
@@ -1,11 +1,6 @@
 #include "IpcCharacterWorldGameMode.h"
 
-#include "Components/StaticMeshComponent.h"
-#include "Engine/DirectionalLight.h"
-#include "Engine/StaticMeshActor.h"
-#include "Engine/World.h"
 #include "IpcCharacterWorldCharacter.h"
-#include "UObject/ConstructorHelpers.h"
 
 AIpcCharacterWorldGameMode::AIpcCharacterWorldGameMode()
 {
@@ -15,20 +10,4 @@ AIpcCharacterWorldGameMode::AIpcCharacterWorldGameMode()
 void AIpcCharacterWorldGameMode::StartPlay()
 {
     Super::StartPlay();
-
-    UWorld* World = GetWorld();
-    if (!World)
-    {
-        return;
-    }
-
-    static ConstructorHelpers::FObjectFinder<UStaticMesh> CubeAsset(TEXT("/Engine/BasicShapes/Cube.Cube"));
-    UStaticMesh* CubeMesh = CubeAsset.Succeeded() ? CubeAsset.Object : nullptr;
-
-    if (CubeMesh)
-    {
-        AStaticMeshActor* Floor = World->SpawnActor<AStaticMeshActor>(FVector(0.0f, 0.0f, -55.0f), FRotator::ZeroRotator);
-        if (Floor)
-        {
-            Floor->GetStaticMeshComponent()->SetStaticMesh(CubeMesh);
-            Floor->SetActorScale3D(FVector(
+}
