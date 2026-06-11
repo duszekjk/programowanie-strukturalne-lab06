f853466bfca06dc342fd5dfd8c596625626f7b01
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-06-11 09:24:06 +0200

    Update IpcLabMode.cpp
    Lab mode not empty

--- Source/IpcCharacterWorld/Private/IpcLabMode.cpp
+++ Source/IpcCharacterWorld/Private/IpcLabMode.cpp
@@ -1,12 +1,39 @@
 #include "IpcLabMode.h"
 
 #include "Camera/CameraActor.h"
-#include "Components/DirectionalLightComponent.h"
-#include "Components/StaticMeshComponent.h"
-#include "Engine/StaticMesh.h"
+#include "Engine/DirectionalLight.h"
 #include "Engine/World.h"
 #include "GameFramework/DefaultPawn.h"
 #include "Kismet/GameplayStatics.h"
-#include "UObject/ConstructorHelpers.h"
 
 AIpcLabMode::AIpcLabMode()
+{
+    DefaultPawnClass = ADefaultPawn::StaticClass();
+}
+
+void AIpcLabMode::StartPlay()
+{
+    Super::StartPlay();
+
+    UWorld* World = GetWorld();
+    if (!World)
+    {
+        return;
+    }
+
+    ACameraActor* Camera = World->SpawnActor<ACameraActor>(
+        FVector(-600.0f, 0.0f, 350.0f),
+        FRotator(-25.0f, 0.0f, 0.0f)
+    );
+
+    APlayerController* PlayerController = UGameplayStatics::GetPlayerController(World, 0);
+    if (Camera && PlayerController)
+    {
+        PlayerController->SetViewTarget(Camera);
+    }
+
+    World->SpawnActor<ADirectionalLight>(
+        FVector(0.0f, 0.0f, 400.0f),
+        FRotator(-45.0f, 0.0f, 0.0f)
+    );
+}
