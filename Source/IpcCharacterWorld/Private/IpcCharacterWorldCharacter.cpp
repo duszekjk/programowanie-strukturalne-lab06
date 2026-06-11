6477aaf00d136d128c429b192706d7cbdfce5f41
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-06-11 10:52:31 +0200

    Make character visible and camera follow it

--- Source/IpcCharacterWorld/Private/IpcCharacterWorldCharacter.cpp
+++ Source/IpcCharacterWorld/Private/IpcCharacterWorldCharacter.cpp
@@ -3,9 +3,11 @@
 #include "Camera/CameraComponent.h"
 #include "CommandReceiverComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
 #include "Engine/Engine.h"
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "UObject/ConstructorHelpers.h"
 
 AIpcCharacterWorldCharacter::AIpcCharacterWorldCharacter()
 {
@@ -14,17 +16,30 @@ AIpcCharacterWorldCharacter::AIpcCharacterWorldCharacter()
     GetCapsuleComponent()->InitCapsuleSize(42.0f, 96.0f);
 
     bUseControllerRotationPitch = false;
-    bUseControllerRotationYaw = false;
+    bUseControllerRotationYaw = true;
     bUseControllerRotationRoll = false;
 
-    GetCharacterMovement()->bOrientRotationToMovement = true;
+    GetCharacterMovement()->bOrientRotationToMovement = false;
     GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
     GetCharacterMovement()->JumpZVelocity = 700.0f;
     GetCharacterMovement()->AirControl = 0.35f;
 
+    BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisibleBody"));
+    BodyMesh->SetupAttachment(RootComponent);
+    BodyMesh->SetRelativeLocation(FVector(0.0f, 0.0f, -45.0f));
+    BodyMesh->SetRelativeScale3D(FVector(0.55f, 0.55f, 1.4f));
+    BodyMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> BodyAsset(TEXT("/Engine/BasicShapes/Capsule.Capsule"));
+    if (BodyAsset.Succeeded())
+    {
+        BodyMesh->SetStaticMesh(BodyAsset.Object);
+    }
+
     CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
     CameraBoom->SetupAttachment(RootComponent);
-    CameraBoom->TargetArmLength = 400.0f;
+    CameraBoom->TargetArmLength = 450.0f;
+    CameraBoom->SetRelativeRotation(FRotator(-20.0f, 0.0f, 0.0f));
     CameraBoom->bUsePawnControlRotation = true;
 
     FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
