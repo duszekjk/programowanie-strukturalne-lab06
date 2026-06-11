d73ef7c4c106242a089165409f811b56b2224386
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-06-11 08:39:00 +0200

    Add Unity character command enum

---
+++ Content/UnityIpcCharacterWorld/Assets/Scripts/CharacterCommand.cs
@@ -0,0 +1,13 @@
+namespace IpcCharacterWorld
+{
+    public enum CharacterCommand
+    {
+        Forward,
+        Backward,
+        Left,
+        Right,
+        Jump,
+        Stop,
+        Reset
+    }
+}
