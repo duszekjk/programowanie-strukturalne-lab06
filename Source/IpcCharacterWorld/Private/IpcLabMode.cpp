9032f9af1924cfb4a6510880845eebc2a16c348a
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-06-11 09:18:19 +0200

    Fix Unreal lab mode camera light and floor spawning

--- Source/IpcCharacterWorld/Private/IpcLabMode.cpp
+++ Source/IpcCharacterWorld/Private/IpcLabMode.cpp
@@ -1,23 +1,12 @@
 #include "IpcLabMode.h"
 
 #include "Camera/CameraActor.h"
+#include "Components/DirectionalLightComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Engine/StaticMesh.h"
 #include "Engine/World.h"
 #include "GameFramework/DefaultPawn.h"
 #include "Kismet/GameplayStatics.h"
+#include "UObject/ConstructorHelpers.h"
 
 AIpcLabMode::AIpcLabMode()
-{
-    DefaultPawnClass = ADefaultPawn::StaticClass();
-}
-
-void AIpcLabMode::StartPlay()
-{
-    Super::StartPlay();
-
-    UWorld* World = GetWorld();
-    if (!World)
-    {
-        return;
-    }
-
-    ACameraActor* Camera = World->SpawnActor<AC
